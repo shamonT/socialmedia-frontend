093f3a232030b551537f3891990608dfb93c30ad
Shamon <shamonp50@gmail.com>
2023-01-18 20:45:03 +0530

    Backend Connected

--- src/api/AuthRequest.js
+++ src/api/AuthRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:3001" });
+const API = axios.create({ baseURL: "https://socialpedia.fashionnova.store/" });
 
 
 export const sendOtp=(email)=>API.post("/send-otp",{email})
